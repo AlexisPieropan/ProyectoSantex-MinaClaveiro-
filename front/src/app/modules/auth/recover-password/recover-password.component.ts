92432a487374f9cb811ceb88eb8209b6321fbf61
kevin joel noviello <kjnoviello@gmail.com>
2023-08-23 23:36:17 -0300

    responsive

--- front/src/app/modules/auth/recover-password/recover-password.component.ts
+++ front/src/app/modules/auth/recover-password/recover-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-recover-password',
